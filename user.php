dcd9b0b21ae43c2d6ebe28f42e3a1cb9fee842d1
GD <gdg@gdg.is>
2016-03-12 17:15:07 +0000

    fikt og prófanir með útlit users

--- user.php
+++ user.php
@@ -1,6 +1,5 @@
 <?php
   if(isset($_GET['boxerID'])) {
-
   $id = $_GET['boxerID'];
   if(!empty($id)){
     require_once('class.sql.php');
@@ -9,16 +8,14 @@
     $boxer_info = '';
     $arr_boxers = $sql_boxer->list_full_boxer_info($id);
     foreach($arr_boxers as $k=>$v){
-        $boxer_info .= "<tr class='clickable-row' data-href='user.php?boxerID=$v[0]' >
-                          <td> $v[0] </td>
-                          <td> $v[1] </td>
-                          <td> $v[2] </td>
-                          <td> $v[3] </td>
-                          <td> $v[4] </td>
-                          <td> $v[5] </td>
-                          <td> $v[6] </td>
-                          <td> $v[7] </td>
-                        </tr>";
+      $id = $v[0];
+      $name = UTF8_encode($v[1]);
+      $kt = $v[2];
+      $phone = $v[3];
+      $email = UTF8_encode($v[4]);
+      $contact_name = UTF8_encode($v[5]);
+      $contact_phone = $v[6];
+      $contact_email = UTF8_encode($v[7]);
     }
     $sql_subscriptions = new SQL();
 
@@ -26,14 +23,7 @@
     if(isset($arr_subs)){
       $subscriptions ='';
       foreach($arr_subs as $k=>$v){
-          $subscriptions .= "<tr>
-                              <td>$v[0]</td>
-                              <td>$v[2]</td>
-                              <td>$v[3]</td>
-                              <td>$v[4]</td>
-                              <td>$v[5]</td>
-                              <td>$v[6]</td>
-                            </tr>";
+          $subscriptions .= "<tr><td>$v[0]</td><td>$v[2]</td><td>$v[3]</td><td>$v[4]</td><td>$v[5]</td><td>$v[6]</td></tr>";
         }
     }
   }
@@ -54,53 +44,79 @@
 
 </head>
 <body>
-  <h2><center> Upplýsingar um iðkanda</center></h2>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Hnefaleikafélag Hafnarfjarðar</a>
+      </div>
+
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#tableBoxers">Yfirlit <span class="sr-only">(current)</span></a></li>
+          <li><a href="#addBoxer" data-toggle="modal" data-target="#addBoxer">Nýskrá iðkanda</a></li>
+          <li><a href="#updatePayment" data-toggle="modal" data-target="#updatePayment">Kaupa Áskrift</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Senda tilkynningu <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#tilkynningar">Senda tilkynningu á netfang eða SMS</a></li>
+              <li class="divider"></li>
+              <li><a href="#contact" data-toggle="modal" data-target="#contact">Senda tilkynningu á vefstjóra</a></li>
+            </ul>
+          </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="http://www.hfh.is">Vefsíða HFH</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <div class="container">
-    <table id="boxerInfo" class="table table-striped table-hover">
-      <thead>
-          <tr>
-              <th>ID</th>
-              <th>Nafn</th>
-              <th>Kennitala</th>
-              <th>Sími</th>
-              <th>Netfang</th>
-              <th>Tengiliður </th>
-              <th>Netfang Tengiliðs</th>
-              <th>Sími Tengiliðs</th>
-          </tr>
-      </thead>
-      <tbody>
-            <?php
-              if(!isset($boxer_info)){
-                print '<p class="text-danger">Engar Upplýsingar fundust um þennan notanda</p>';
-              } else {
-                  print UTF8_encode($boxer_info);
-              } ?>
-      </tbody>
-    </table>
-  </div>
-  <h3><center> Greiðsluupplýsingar</center></h3>
-  <div class="container">
-    <table id="boxerInfo" class="table table-striped table-hover">
-      <thead>
-          <tr>
-              <th>ID</th>
-              <th>Hópur</th>
-              <th>Greiðsluaðferð</th>
-              <th>Lengd skráningar</th>
-              <th>Keypt þann</th>
-              <th>Rennur út</th>
-          </tr>
-      </thead>
-      <tbody>
-            <?php
-              if(!isset($subscriptions)){
-                print '<p class="text-danger">Engar Greiðsluupplýsingar fundust um þennan iðkanda</p>';
-              } else {
-                print UTF8_encode($subscriptions);
-              }?>
-      </tbody>
-    </table>
+    <div class="col-md-3">
+      <img src='static/img/empty-img.png' width='' height='300'/>
+    </div>
+    <div class="col-md-9">
+        <?php
+          if(!isset($boxer_info)){
+            print '<h2 class="text-danger">Engar Upplýsingar fundust um þennan notanda</h2>';
+          } else {
+            print "
+            <h3> $name </h3>
+            <ul>
+              <li>Kennitala: $kt </li>
+              <li>Sími: $phone </li>
+              <li>Netfang: $email </li>
+            </ul>";
+           } ?>
+    </div>
+    <div class="col-md-12">
+      <h3><center> Greiðsluupplýsingar</center></h3>
+      <table id="boxerInfo" class="table table-striped table-hover">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Hópur</th>
+                <th>Greiðsluaðferð</th>
+                <th>Lengd skráningar</th>
+                <th>Keypt þann</th>
+                <th>Rennur út</th>
+            </tr>
+        </thead>
+        <tbody>
+              <?php
+                if(!isset($subscriptions)){
+                  print '<p class="text-danger">Engar Greiðsluupplýsingar fundust um þennan iðkanda</p>';
+                } else {
+                  print UTF8_encode($subscriptions);
+                }?>
+        </tbody>
+      </table>
+    </div>
   </div>
 </body
 </html>
