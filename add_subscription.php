85ea1adae2833935a7ae70b24458af0def48ddec
gunnidg <gdg@gdg.is>
2016-04-16 19:24:05 +0000

    komið skilirði fyrir rettu formi i add_subscription

--- add_subscription.php
+++ add_subscription.php
@@ -18,7 +18,8 @@
       }
   }*/
 
-//  if(isset($_POST['addSubscription'])) {
+  if(isset($_POST['form_name'])) {
+    //addSubscription
     $sql_add_subscription = new SQL;
     $id = $_POST['boxer_id'];
     $group_id = $_POST['group_id'];
@@ -27,12 +28,12 @@
     $begin_date = date("Y-m-d", strtotime($_POST['begin_date']));
     $end_date = date("Y-m-d", strtotime($_POST['end_date']));
     //print "$id , $group_id , $paymentType_id , $subscriptionType_id , $begin_date , $end_date";
-    if($sql_add_subscription->add_subscription($id, $group_id, $paymentType_id, $subscriptionType_id, $begin_date, $end_date)) {
-      /*$result = array("success" => 1, "id" => $id, "group_id" => $group_id, "paymentType_id" => $paymentType_id, "subscriptionType_id" => $subscriptionType_id, "begin_date" => $begin_date, "end_date" => $end_date);*/
+    //if($sql_add_subscription->add_subscription($id, $group_id, $paymentType_id, $subscriptionType_id, $begin_date, $end_date)) {
+    if($sql_add_subscription->add_subscription($_POST['boxer_id'], $_POST['group_id'], $_POST['paymentType_id'], $_POST['subscriptionType_id'], date("Y-m-d", strtotime($_POST['begin_date'])), date("Y-m-d", strtotime($_POST['end_date'])))) {
       $result = 1;
     } else {
       $result = array("success" => 0);
       }
       echo json_encode($result);
-//  }
+  }
 ?>
--- js/script.js
+++ js/script.js
@@ -1,47 +1,16 @@
-/*$(document).ready(function(){
-$("#submit").click(function(){
-var boxer_id = $("#boxer_id").val();
-var group_id = $("#group_id").val();
-var paymentType_id = $("#paymentType_id").val();
-var subscriptionType_id = $("#subscriptionType_id").val();
-var begin_date = $("#begin_date").val();
-var end_date = $("#end_date").val();
-// Returns successful data submission message when the entered information is stored in database.
-  var dataString = 'boxer_id='+ boxer_id + '&group_id='+ group_id + '&paymentType_id='+ paymentType_id + '&subscriptionType_id='+ subscriptionType_id + '&begin_date=' + begin_date + '&end_date=' + end_date;
-if(group_id==''||paymentType_id==''||subscriptionType_id==''||begin_date==''||end_date=='') {
-  alert("Please Fill All Fields");
-}
-else
-{
-// AJAX Code To Submit Form.
-  $.ajax({
-  type: "POST",
-  url: "add_subscription.php",
-  data: dataString,
-  cache: false,
-  success: function(result){
-    $('#addSubscription').modal('hide')
-    if(result == 1){
-      $('#addSubscription').modal('hide')
-      alert(result);
-
-    }
-    //$('#addSubscription').modal('hide');
-  }
-});
-}
-return false;
-});
-});*/
 src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"
 
 $(document).ready(function(){
   $('form#addSubscription').on('submit', function() {
     var form = $(this);
     event.preventDefault();
+    console.log(form.serialize());
+    var form_name = 'addSubscription';
+    var data = "form_name=" + form_name +"&" + form.serialize();
+    console.log(data);
     $.ajax({
       url: form.attr('action'),
-      data: form.serialize(),
+      data: data,
       method:'POST',
       success: function(result){
         if(result == 1){
@@ -55,4 +24,5 @@ $(document).ready(function(){
     });
 
   });
+  function formSuccess
 });
