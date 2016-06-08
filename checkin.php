f746cb6bee6b90e4c4336931c7887b12f17b5175
GD <gunnarg15@ru.is>
2016-06-08 20:05:30 +0000

    lagadi utlit a checkin og bjo til klasann CheckIn

--- checkin.php
+++ checkin.php
@@ -11,19 +11,20 @@ $pageTitle = "Innskráning";
 include_once "common/head.php";
 include_once "common/scripts.php";
 ?>
+<div id="bg">
+    <img src="img/HFHLogo.png" alt="HFH Logo" />
+</div>
 <div class="container login-window">
     <div class="col-lg-12 ">
-        <!--<img src="img/HFHLogo.png" alt="HFH Logo" />-->
-
         <form id="checkIn" method="POST" action="">
             <div class="row">
-            <fieldset class="form-group col-xs-4 col-xs-offset-4">
+            <fieldset class="form-group col-sm-4 col-sm-offset-4 col-xs-8 col-xs-offset-2">
                 <label for="inputID">Vinsamlegast skráðu þig inn</label>
                 <input type="text" class="form-control" id="inputID" placeholder="kt">
             </fieldset>
             </div>
             <div class="row">
-            <button type="submit" class="btn btn-primary col-xs-4 col-xs-offset-4">Innskrá</button>
+            <button type="submit" class="btn btn-primary col-sm-4 col-sm-offset-4 col-xs-8 col-xs-offset-2">Innskrá</button>
             </div>
         </form>
     </div>
