4e0d50bd342aea629a06c1f1e9a3e7f39cb6fe60
GD <gunnarg15@ru.is>
2016-06-13 16:46:13 +0000

    continuing work on changing from mysqli to PDO, now in index

--- class.sql.php
+++ class.sql.php
@@ -8,6 +8,36 @@
 	 * @version 0.0.3
 	 * @todo
 	 */
+    class newSQL
+    {
+        private $_db;
+
+        /**
+         * Checks for a database object and creates one if none is found
+         *
+         * @param object $db
+         * @return void
+         */
+        public function __construct($db=NULL) {
+            if(is_object($db)) {
+                $this->_db = $db;
+            }
+            else {
+                $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME;
+                $this->_db = new PDO($dsn, DB_USER, DB_PASS);
+            }
+        }
+
+        public function list_boxers(){
+            $sql = "call list_boxers()";
+            if($stmt = $this->_db->prepare($sql)){
+                $stmt->execute();
+                $row = $stmt->fetchAll();
+                return $row;
+            }
+            return false;
+        }
+    }
 	class SQL
 	{
 		private $connection;
--- index.php
+++ index.php
@@ -1,27 +1,15 @@
 <?php
-require_once('class.sql.php');
+include_once "common/base.php";
+$pageTitle = "Iðkennda yfirlit";
+include_once "class.sql.php";
+include_once "common/head.php";
+include_once "common/scripts.php";
 
-  $sql_boxers = new SQL();
-  if(isset($_POST['addBoxer'])) {
-    $sql_add_boxer = new SQL;
-    $name = $_POST['name'];
-    $kt = $_POST['kt'];
-    $phone = $_POST['phone'];
-    $email = $_POST['email'];
-    $contact_name = $_POST['contact_name'];
-    $contact_phone = $_POST['contact_phone'];
-    $contact_email = $_POST['contact_email'];
-    if($sql_add_boxer->add_boxer(utf8_decode($name), utf8_decode($kt), utf8_decode($phone), utf8_decode($email), utf8_decode($contact_name), utf8_decode($contact_phone), utf8_decode($contact_email))) {
-      print ('<div class="alert alert-dismissible alert-success">  <button type="button" class="close" data-dismiss="alert">x</button>  <strong>Iðkandi hefur verið skráður</strong>  </div>');
-    } else {
-        print ('<div class="alert alert-dismissible alert-danger">  <button type="button" class="close" data-dismiss="alert">x</button>  <strong>Obbosí!</strong> einhvað fór úrskeiðis, reyndu aftur.  </div>');
-      }
-  }
-
-  $arr_boxers = $sql_boxers->list_boxers();
-  if(isset($arr_boxers )){
+$newSQL = new newSQL();
+$arrayOfBoxers = $newSQL->list_boxers();
+if($arrayOfBoxers != false){
     $boxers_list = '';
-    foreach($arr_boxers as $k=>$v){
+    foreach($arrayOfBoxers as $k=>$v){
         $boxers_list .= "<tr>
                           <td><a href='user.php?boxerID=$v[0]'><strong> $v[1] </strong></a></td>
                           <td> $v[2] </td>
@@ -30,43 +18,39 @@ require_once('class.sql.php');
                         </tr>";
     }
 }
-
-$pageTitle = "Heim";
-include_once "common/head.php";
 include_once "common/nav-def.php";
-include_once "common/scripts.php";
 ?>
 
     <h1><center><img src="img/HFHLogo-192x192.png" alt="hfh logo"> Yfirlitskerfi Hnefaleikafélags Hafnarfjarðar </center></h1>
-<div class="container">
-  <table id="boxersTable" class="table table-striped table-hover">
-    <thead>
-        <tr>
-            <th>Nafn</th>
-            <th>Kennitala</th>
-            <th>Sími</th>
-            <th>Netfang</th>
-        </tr>
-    </thead>
-    <tbody>
-    <?php
-      if(!isset($boxers_list)){
-        print '<p class="text-danger">There was a problem connecting to the database, contact administrator</p>';
-      } else {
-          print UTF8_encode($boxers_list);
-      }
-    ?>
-    </tbody>
-  </table>
-</div>
+    <div class="container">
+        <table id="boxersTable" class="table table-striped table-hover">
+            <thead>
+            <tr>
+                <th>Nafn</th>
+                <th>Kennitala</th>
+                <th>Sími</th>
+                <th>Netfang</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            if(!$arrayOfBoxers){
+                print '<p class="text-danger">There was a problem connecting to the database, contact administrator</p>';
+            } else {
+                print UTF8_encode($boxers_list);
+            }
+            ?>
+            </tbody>
+        </table>
+    </div>
 
-    
+
 
     <script>
-      $(document).ready(function() {
-        $('#boxersTable').DataTable();
-      } );
-      </script>
+        $(document).ready(function() {
+            $('#boxersTable').DataTable();
+        } );
+    </script>
 
 <?php
 include_once "common/footer.php";
