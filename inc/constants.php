1d397648d5aed5a8a2b88a38b8bd92109de124c4
gdgunnars <gdg@gdg.is>
2016-06-29 15:28:25 +0000

    db file

---
+++ inc/constants.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gd
+ * Date: 12.6.2016
+ * Time: 02:23
+ */
+
+
+// Database credentials
+define('DB_HOST', 'db-server');
+define('DB_USER', 'db-user');
+define('DB_PASS', 'db-pass');
+define('DB_NAME', 'db-name');
+
+?>
