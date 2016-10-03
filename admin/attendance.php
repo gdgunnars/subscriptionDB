195d8fa54580eace743d8429ec7c3b68d73c7639
GD <gunnarg15@ru.is>
2016-10-03 13:50:36 +0000

    Changed the layout of attendance and also got the last 2 days of attendance

--- admin/attendance.php
+++ admin/attendance.php
@@ -17,9 +17,10 @@ $newSQL = new newSQL();
 
     $yesterDay = date('Y-m-d',strtotime("-1 days"));
     $currDate = date('Y-m-d');
-    $boxers_list = $newSQL->list_structured_attendance($currDate);
-    $oldBoxer_list = $newSQL->list_structured_attendance($yesterDay);
-    $framhalds = $newSQL->list_structured_attendance_for_group($currDate, utf8_decode('Framhaldshópur'));
+    $attendanceToday = $newSQL->list_structured_attendance($currDate);
+    $attendanceYesterday = $newSQL->list_structured_attendance($yesterDay);
+    $attendance2DaysAgo = $newSQL->list_structured_attendance(date('Y-m-d',strtotime("-2 days")));
+    //$framhalds = $newSQL->list_structured_attendance_for_group($currDate, utf8_decode('Framhaldshópur'));
     include_once (fullDirPath . "/head.php");
     include_once (fullDirPath . "/nav-def.php");
 
@@ -27,56 +28,6 @@ $newSQL = new newSQL();
 ?>
 
     <div class="container">
-        <div class="col-sm-6">
-            <div class="well">
-                <h3> <strong><?php echo date('l d M Y'); ?></strong></h3>
-                <?php echo 'Week: ' . date('W') . ' - Day: ' . date('z') ?>
-                <br /><br />
-                <table id="boxersTable" class="table table-striped table-hover">
-                    <thead>
-                    <tr>
-                        <th>Nafn</th>
-                        <th>Mætti kl:</th>
-                        <th>Hópur</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php
-                    if(!$boxers_list){
-                        print '<p class="text-danger">Enginn hefur skráð sig inn ennþá</p>';
-                    } else {
-                        print UTF8_encode($boxers_list);
-                    }
-                    ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="well">
-                <h3> <strong><?php echo date('l d M Y', strtotime("-1 days")); ?></strong></h3>
-                <?php echo 'Week: ' . date('W', strtotime("-1 days")) . ' - Day: ' . date('z', strtotime("-1 days")) ?>
-                <br /><br />
-                <table id="boxersTable" class="table table-striped table-hover">
-                    <thead>
-                    <tr>
-                        <th>Nafn</th>
-                        <th>Mætti kl:</th>
-                        <th>Hópur</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php
-                    if(!$oldBoxer_list){
-                        print '<p class="text-danger">Enginn skráði sig inn í gær</p>';
-                    } else {
-                        print UTF8_encode($oldBoxer_list);
-                    }
-                    ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
         <div class="col-sm-12">
             <div class="well">
                 <h3> <strong><?php echo date('l d M Y'); ?></strong></h3>
@@ -92,17 +43,68 @@ $newSQL = new newSQL();
                     </thead>
                     <tbody>
                     <?php
-                    if(!$framhalds){
+                    if(!$attendanceToday){
                         print '<p class="text-danger">Enginn hefur skráð sig inn ennþá</p>';
                     } else {
-                        print UTF8_encode($framhalds);
+                        print UTF8_encode($attendanceToday);
                     }
                     ?>
                     </tbody>
                 </table>
+            </div>
         </div>
-    </div>
+        <div class="col-sm-6">
+            <div class="well">
+                <h3> <strong><?php echo date('l d M Y', strtotime("-1 days")); ?></strong></h3>
+                <?php echo 'Week: ' . date('W', strtotime("-1 days"))  . ' - Day: ' . date('z', strtotime("-1 days")) ?>
+                <br /><br />
+                <table id="boxersTable" class="table table-striped table-hover">
+                    <thead>
+                    <tr>
+                        <th>Nafn</th>
+                        <th>Mætti kl:</th>
+                        <th>Hópur</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    if(!$attendanceYesterday){
+                        print '<p class="text-danger">Enginn hefur skráð sig inn ennþá</p>';
+                    } else {
+                        print UTF8_encode($attendanceYesterday);
+                    }
+                    ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="well">
+                <h3> <strong><?php echo date('l d M Y', strtotime("-2 days")); ?></strong></h3>
+                <?php echo 'Week: ' . date('W', strtotime("-2 days")) . ' - Day: ' . date('z', strtotime("-2 days")) ?>
+                <br /><br />
+                <table id="boxersTable" class="table table-striped table-hover">
+                    <thead>
+                    <tr>
+                        <th>Nafn</th>
+                        <th>Mætti kl:</th>
+                        <th>Hópur</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    if(!$attendance2DaysAgo){
+                        print '<p class="text-danger">Enginn skráði sig inn í gær</p>';
+                    } else {
+                        print UTF8_encode($attendance2DaysAgo);
+                    }
+                    ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
 
+    </div>
 
 <?php
 
