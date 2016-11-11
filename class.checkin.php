b6136761c5cf5ea84e1690e923ef41b242c6e165
gdgunnars <gdg@gdg.is>
2016-11-11 09:55:59 +0000

    extended fix on previous error

--- class.checkin.php
+++ class.checkin.php
@@ -98,11 +98,11 @@ class CheckIn
 
         $userInfo = $this->get_info($id['ID']);
         $image = 'admin/'.$userInfo['image'];
-        if(empty($image))
+        if(empty($imge))
             $image = "img/No-image-available.png";
         $name = utf8_encode($userInfo['Name']);
 
-        $returnMsg = '<div class="checkin"> <img src=\' .$image. '\' width=\'450\'">
+        $returnMsg = '<div class="checkin"> <img src=\'' .$image. '\' width=\'450\'">
             <h2> Velkomin/n, '. $name .' </h2>
             <h3> Þú hefur verið skráð/ur inn</h3></div>';
         $returnArray=array(
